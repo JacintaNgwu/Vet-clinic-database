3c5c11599bd796b95897b5f82d1de11066e27a26
JacintaNgwu <jacintangwu9900@gmail.com>
2023-03-01 11:13:07 +0530

    Added more animals data

--- data.sql
+++ data.sql
@@ -1,13 +1,18 @@
 /* Populate database with sample data. */
 
 INSERT INTO animals (name, date_of_birth, neutered, weight_kg, escape_attempts)
-VALUES ('Agumon', '2020-02-3', 'Yes', 10.23, 0);
+VALUES 
+ ('Agumon', '2020-02-3', 'Yes', 10.23, 0),
+ ('Gabumon', '2018-11-15', 'Yes', 8, 2),
+ ('Pikachu.', '2021-01-7', 'No', 15.04, 1),
+ ('Devimon', '2017-05-12', 'Yes', 11, 5);
 
 INSERT INTO animals (name, date_of_birth, neutered, weight_kg, escape_attempts)
-VALUES ('Gabumon', '2018-11-15', 'Yes', 8, 2);
-
-INSERT INTO animals (name, date_of_birth, neutered, weight_kg, escape_attempts)
-VALUES ('Pikachu.', '2021-01-7', 'No', 15.04, 1);
-
-INSERT INTO animals (name, date_of_birth, neutered, weight_kg, escape_attempts)
-VALUES ('Devimon', '2017-05-12', 'Yes', 11, 5);
+VALUES 
+('Charmander', '2020-02-08', 'No', -11, 0),
+('Plantmon', '2021-11-15', 'Yes', -5.7, 2),
+('Squirtle', '1993-04-02', 'No', -12.13, 3),
+('Angemon', '2005-06-12', 'Yes', -45, 1),
+('Boarmon', '2005-06-07', 'Yes', 20.4, 7),
+('Blossom', '1998-10-13', 'Yes', 17, 3),
+('Ditto', '2022-05-14', 'Yes', 22, 4);
