d9e243a6539ac95daf430b319feacc0a292db0f3
JacintaNgwu <jacintangwu9900@gmail.com>
2023-03-02 13:16:30 +0530

    Implemented many-many relationship

--- schema.sql
+++ schema.sql
@@ -30,3 +30,22 @@ ALTER TABLE animals ADD FOREIGN KEY (species_id) REFERENCES species(id);
 
 ALTER TABLE animals ADD COLUMN owner_id INTEGER;
 ALTER TABLE animals ADD FOREIGN KEY (owner_id) REFERENCES owners(id);
+
+CREATE TABLE vet(
+id SERIAL PRIMARY KEY,
+name VARCHAR(255),
+age INT,
+date_of_graduation DATE
+);
+
+CREATE TABLE specializations(
+vet_id INT REFERENCES vet(id),
+species_id INT REFERENCES species(id),
+PRIMARY KEY (vet_id, species_id)
+);
+
+CREATE TABLE visits(
+animal_id INT REFERENCES animals(id),
+vet_id INT REFERENCES vet(id),
+visit_date DATE
+);
